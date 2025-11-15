ef28f77ed1058249ca07e9d9bc517e15ad75b00f
Leonardo22sp <santos.leonardo22sp@gmail.com>
2025-11-15 20:01:39 -0300

    teste de armazenamento 2

--- school_manager/settings.py
+++ school_manager/settings.py
@@ -90,17 +90,6 @@ DATABASES = {
 }
 
 # ==============================
-# Validação de senha
-# ==============================
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
-# ==============================
 # Internacionalização
 # ==============================
 
